c49de77db1b1c878881ddefc33363d24e58812d0
musl <132400428+muslll@users.noreply.github.com>
2023-09-02 19:50:51 +0000

    fix amp log loop

--- neosr/models/default.py
+++ neosr/models/default.py
@@ -222,10 +222,12 @@ class default():
 
         if self.opt['use_amp'] is True:
             use_amp = True
-            logger = get_root_logger()
-            logger.info('AMP enabled.')
         if self.opt['bfloat16'] is True:
             amp_dtype = torch.bfloat16
+
+        if use_amp is True: 
+            logger = get_root_logger()
+            logger.info('AMP enabled.')
 
         scaler = torch.cuda.amp.GradScaler(enabled=use_amp)
 
