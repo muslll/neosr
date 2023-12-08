0c9099f71336b8956705e28f6e40037d051d7877
musl <132400428+muslll@users.noreply.github.com>
2023-12-08 01:19:45 +0000

    add bpp_l preset

--- neosr/archs/bpp_arch.py
+++ neosr/archs/bpp_arch.py
@@ -32,3 +32,35 @@ class bpp(nn.Module):
         y = self.conv_out(x2)
         y = self.Depth2Space(y)
         return y
+
+
+@ARCH_REGISTRY.register()
+class bpp_l(nn.Module):
+    def __init__(self, sr_rate=upscale):
+        super(bpp_l, self).__init__()
+        self.conv0 = nn.Conv2d(3, 34, kernel_size=3, stride=2, padding=1, bias=True)
+        self.conv1 = nn.Conv2d(34, 34, kernel_size=3, padding=1, bias=True)
+        self.conv2 = nn.Conv2d(34, 34, kernel_size=3, padding=1, bias=True)
+        self.conv_out = nn.Conv2d(34, (2*sr_rate)**2 * 3, kernel_size=3, padding=1, bias=False)
+        self.Depth2Space = nn.PixelShuffle(2*sr_rate)
+        self.act = nn.LeakyReLU(inplace=True, negative_slope=0.1)
+
+    def forward(self, x):
+        x0 = self.conv0(x)
+        x0 = self.act(x0)
+        x1 = self.conv1(x0)
+        x1 = self.act(x1)
+        # double
+        x1 = self.conv1(x0)
+        x1 = self.act(x1)
+
+        x2 = self.conv2(x1)
+        x2 = self.act(x2) + x0
+
+        # double
+        x2 = self.conv2(x1)
+        x2 = self.act(x2) + x0
+
+        y = self.conv_out(x2)
+        y = self.Depth2Space(y)
+        return y
