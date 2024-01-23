18076d152d198dd26d3f1e3792ba95275e488da0
musl <132400428+muslll@users.noreply.github.com>
2024-01-22 21:45:21 -0300

    adjust span and ruff config, remove swinir bloat

--- neosr/archs/swinir_arch.py
+++ neosr/archs/swinir_arch.py
@@ -967,30 +967,6 @@ class swinir(nn.Module):
         flops += self.upsample.flops()
         return flops
 
-
-if __name__ == '__main__':
-    upscale = 4
-    window_size = 8
-    height = (1024 // upscale // window_size + 1) * window_size
-    width = (720 // upscale // window_size + 1) * window_size
-    model = swinir(
-        upscale=2,
-        img_size=(height, width),
-        window_size=window_size,
-        img_range=1.,
-        depths=[6, 6, 6, 6],
-        embed_dim=60,
-        num_heads=[6, 6, 6, 6],
-        mlp_ratio=2,
-        upsampler='pixelshuffledirect')
-    print(model)
-    print(height, width, model.flops() / 1e9)
-
-    x = torch.randn((1, 3, height, width))
-    x = model(x)
-    print(x.shape)
-
-
 @ARCH_REGISTRY.register()
 def swinir_small(**kwargs):
     return swinir(
