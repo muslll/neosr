bf3b052d0e52e510cfa6c052f5b0ffcb03e2fcd1
Crustaceous D <31586082+terrainer@users.noreply.github.com>
2024-02-28 13:03:03 +1100

    revert adaml to default code

--- neosr/optimizers/adaml_optim.py
+++ neosr/optimizers/adaml_optim.py
@@ -1,7 +1,9 @@
+import math
 import torch
 from torch.optim.optimizer import Optimizer
 
 from neosr.utils.registry import OPTIMIZER_REGISTRY
+
 
 @OPTIMIZER_REGISTRY.register()
 class AdamL(Optimizer):
@@ -14,25 +16,27 @@ class AdamL(Optimizer):
     Parameters:
     - params (iterable): Iterable of parameters to optimize or dictionaries defining parameter groups.
     - lr (float, optional): Learning rate (default: 1e-3).
-    - betas (Tuple[float, float], optional): Coefficients used for computing running averages (default: (0.9, 0.999)).
+    - betas (Tuple[float, float], optional): Coefficients used for computing running averages of gradient and its square (default: (0.9, 0.999)).
     - eps (float, optional): Term added to the denominator to improve numerical stability (default: 1e-8).
     - weight_decay (float, optional): Weight decay (L2 penalty) (default: 0).
     """
+
     def __init__(self, params, lr=1e-3, betas=(0.9, 0.999), eps=1e-8, weight_decay=0):
         defaults = dict(lr=lr, betas=betas, eps=eps, weight_decay=weight_decay)
         super(AdamL, self).__init__(params, defaults)
+
         self.betas = betas
         self.beta_t = betas[0] ** 0  # Initialize beta1 to 1
         self.N_sma_max = 2 / (1 - betas[1]) - 1  # Compute max length for simple moving average (SMA)
         self.steps = 0  # Initialize step counter
 
-    def step(self, closure=None):
+    def step(self, loss, closure=None):
         """
         Performs a single optimization step (parameter update).
+
         Parameters:
         - closure (callable, optional): A closure that reevaluates the model and returns the loss.
         """
-        loss = None
         if closure is not None:
             loss = closure()
 
@@ -40,13 +44,16 @@ class AdamL(Optimizer):
             for p in group['params']:
                 if p.grad is None:
                     continue  # Skip update if gradient is not available
+
                 # Initialize or retrieve state
                 state = self.state[p]
                 if len(state) == 0:
                     state['step'] = 0
                     state['exp_avg'] = torch.zeros_like(p.data)
                     state['exp_avg_sq'] = torch.zeros_like(p.data)
+
                 state['step'] += 1  # Increment step counter
+
                 # Update biased first moment estimate
                 exp_avg = state['exp_avg']
                 beta1 = self.beta_t
