f67bc742122ed46ea44b0d16e61c7554fc58dc37
marcobarbierato <132559008+marcobarbierato@users.noreply.github.com>
2024-05-13 15:43:07 +0200

    Update desrgan_arch.py

--- neosr/archs/desrgan_arch.py
+++ neosr/archs/desrgan_arch.py
@@ -67,19 +67,19 @@ class ResidualDenseDeformableBlock(nn.Module):
                                      1,
                                      1,
                                      bias=True)
-        nn.init.constant_(self.offset_conv2.weight, 0.)
-        nn.init.constant_(self.offset_conv2.bias, 0.)
+        nn.init.constant_(self.offse4.weight, 0.)
+        nn.init.constant_(self.offset4.bias, 0.)
         
         self.conv5 = nn.Conv2d(num_feat + 4 * num_grow_ch, num_feat, 3, 1, 1)
 
-        self.offset_conv5 = nn.Conv2d(num_feat + 4 * num_grow_ch,
+        self.offset5 = nn.Conv2d(num_feat + 4 * num_grow_ch,
                                      2 * 3 * 3,
                                      3,
                                      1,
                                      1,
                                      bias=True)
-        nn.init.constant_(self.offset_conv5.weight, 0.)
-        nn.init.constant_(self.offset_conv5.bias, 0.)
+        nn.init.constant_(self.offset5.weight, 0.)
+        nn.init.constant_(self.offset5.bias, 0.)
         
 
         self.lrelu = nn.LeakyReLU(negative_slope=0.2, inplace=True)
@@ -95,17 +95,21 @@ class ResidualDenseDeformableBlock(nn.Module):
         x1= deform_conv2d(x, offset=self.offset1(x), weight=self.conv1.weight,
                                           bias=self.conv1.bias,stride=1,padding=1)
         #x2 = self.lrelu(self.conv2(torch.cat((x, x1), 1)))
-        x2= deform_conv2d(x, offset=self.offset2(x), weight=self.conv2.weight,
-                                          bias=self.conv2.bias,stride=1,padding=1)
+        o1 = torch.cat((x, x1), 1))
+        x2= self.lrelu( deform_conv2d(o1, offset=self.offset2(o1), weight=self.conv2.weight,
+                                          bias=self.conv2.bias,stride=1,padding=1) )
         #x3 = self.lrelu(self.conv3(torch.cat((x, x1, x2), 1)))
-        x3= deform_conv2d(x, offset=self.offset3(x), weight=self.conv3.weight,
-                                          bias=self.conv3.bias,stride=1,padding=1)
+        o2 = torch.cat((x, x1, x2), 1)
+        x3= self.lrelu( deform_conv2d(o2, offset=self.offset3(o2), weight=self.conv3.weight,
+                                          bias=self.conv3.bias,stride=1,padding=1))
         #x4 = self.lrelu(self.conv4(torch.cat((x, x1, x2, x3), 1)))
-        x4= deform_conv2d(x, offset=self.offset4(x), weight=self.conv4.weight,
-                                          bias=self.conv4.bias,stride=1,padding=1)
+        o3 = torch.cat((x, x1, x2, x3), 1)
+        x4= self.lrelu(deform_conv2d(o3, offset=self.offset4(o3), weight=self.conv4.weight,
+                                          bias=self.conv4.bias,stride=1,padding=1))
         #x5 = self.conv5(torch.cat((x, x1, x2, x3, x4), 1))
-        x5= deform_conv2d(x, offset=self.offset5(x), weight=self.conv5.weight,
-                                          bias=self.conv5.bias,stride=1,padding=1)
+        o4 = torch.cat((x, x1, x2, x3, x4), 1
+        x5= self.lrelu( deform_conv2d(o4, offset=self.offset5(o4), weight=self.conv5.weight,
+                                          bias=self.conv5.bias,stride=1,padding=1) )
         # Empirically, we use 0.2 to scale the residual for better performance
         return x5 * self.scale_residual + x
 
