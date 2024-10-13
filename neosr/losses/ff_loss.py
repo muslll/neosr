4fa92a9fe3604b298e715b7187afcba43878312f
musl <132400428+muslll@users.noreply.github.com>
2024-10-13 03:52:20 -0300

    add lambda value to focalfrequency to balance it against other losses

--- neosr/losses/ff_loss.py
+++ neosr/losses/ff_loss.py
@@ -140,11 +140,8 @@ class ff_loss(nn.Module):
             pred_freq = torch.mean(pred_freq, 0, keepdim=True)
             target_freq = torch.mean(target_freq, 0, keepdim=True)
 
-        '''
-        # avoid out of proportion weight
-        loss = loss / torch.std(loss)
-        loss = torch.mean(loss) * 0.5
-        '''
-
         # calculate focal frequency loss
-        return self.loss_formulation(pred_freq, target_freq, matrix) * self.loss_weight
+        # lambda 50 to increase weight compared to other losses
+        loss = self.loss_formulation(pred_freq, target_freq, matrix) * 50
+
+        return loss * self.loss_weight
